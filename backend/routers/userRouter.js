ede1f518578b7bb35bc8699a35473f8193a3e6fa
Pawel Gurlo <pawel.gurlo@gmail.com>
2026-01-07 01:05:55 +0100

    Fix: Add authentication to comment post route

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { getAllBlogs, getBlog, postComment } from "../controllers/userController.js";
+import { authenticate } from "../middlewares/auth.js";
 
 const userRouter = new Router();
 
 userRouter.get("/blogs", getAllBlogs);
 userRouter.get("/blogs/:id", getBlog);
-userRouter.post("blogs/:id/comments", postComment);
+userRouter.post("blogs/:id/comments", authenticate, postComment);
 
 export default userRouter;
